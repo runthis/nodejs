203991509502ade40a08a258da033c0f0cdd0b8f
Robert Miller <robertjessemiller@gmail.com>
2022-11-17 10:35:10 -0600

    commit

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const longString = Array(16000).fill(0).map(x => Math.random().toString(36).charAt(2)).join('');
+
 app.get('/', async (req, res) => {
-    res.send("Hello World")
+    res.send(longString)
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`Hello World, Application is running on port ${process.env.PORT}`)
+    console.log(`Hello World Application is running on port ${process.env.PORT}`)
 })
