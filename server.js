3c3fb98dcadbc362a0e7e5edb0d724949e73928d
Robert Miller <robertjessemiller@gmail.com>
2022-11-17 10:43:40 -0600

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
 app.get('/', async (req, res) => {
-    res.send(Array(16000).fill(0).map(x => Math.random().toString(36).charAt(2)).join(''))
+    res.send(Array(16000).fill(0).map(x => Math.random().toString(36).charAt(2)).join(''));
 })
