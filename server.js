5db3c58e081dc4a43c7efc2a35be5fe0f98eee13
Robert Miller <robertjessemiller@gmail.com>
2022-11-17 10:39:38 -0600

    commit

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const longString = Array(16000).fill(0).map(x => Math.random().toString(36).charAt(2)).join('');
-
 app.get('/', async (req, res) => {
-    res.send(longString)
+    res.send(Array(16000).fill(0).map(x => Math.random().toString(36).charAt(2)).join(''))
 })
 
 app.listen(process.env.PORT, () => {
